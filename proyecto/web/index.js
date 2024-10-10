4d0b25699057d19bc8230c3836ef5a7c3bd84d71
AXL GAMAS <axlgamas@outlook.com>
2024-10-10 13:03:39 -0600

    cambios

---
+++ proyecto/web/index.js
@@ -0,0 +1,14 @@
+const formDatos = document.getElementById("datos");
+let datoArr=[]
+formDatos.addEventListener("submit", (ev) => {
+    ev.preventDefault()
+    const nombre = document.getElementById("nombre").value;
+    const edad = document.getElementById("edad").value;
+    const sexo = document.getElementById("sexo").value;
+    const rfc = document.getElementById("rfc").value;
+
+
+    let info = { nombre: nombre, edad: edad, sexo: sexo, rfc: rfc }
+    datoArr.push(info)
+    console.log(datoArr);
+})
